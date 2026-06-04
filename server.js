7d0497cc25ca9598d92a794768a4c2161987db13
pranith00077 <pranithkumar931@gmail.com>
2026-06-04 14:39:20 +0530

    Initial commit

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
